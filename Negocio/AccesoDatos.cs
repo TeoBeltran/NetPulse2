70dbee805464184c4598a5d43b90e15fe5d5ea2c
Teo Beltran <teo.belnan@gmail.com>
2024-08-12 01:38:37 -0300

    .

---
+++ Negocio/AccesoDatos.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class AccesoDatos
+    {
+        public SqlConnection conection;
+        public SqlDataReader lector;
+        public SqlCommand cmd;
+        public AccesoDatos()
+        {
+            conection = new SqlConnection("server=.\\SQLEXPRESS;database=BD_NetPulse;integrated security=true");
+            cmd = new SqlCommand();
+
+        }
+        public SqlDataReader Lector
+        {
+            get { return lector; }
+        }
+
+        public void setConsulta(string consulta)
+        {
+            cmd.CommandType = System.Data.CommandType.Text;
+            cmd.CommandText = consulta;
+        }
+        public void setearParametro(string nombre, object valor)
+        {
+            cmd.Parameters.AddWithValue(nombre, valor);
+        }
+        public void ejecutarLectura()
+        {
+            cmd.Connection = conection;
+            try
+            {
+                conection.Open();
+                lector = cmd.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Se produjo un error: " + ex.ToString());
+                throw ex;
+            }
+
+        }
+        public int ejecutarAccion()
+        {
+            cmd.Connection = this.conection;
+            int rowsAffected = 0;
+            try
+            {
+                conection.Open();
+                rowsAffected = cmd.ExecuteNonQuery();
+                return rowsAffected;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public void cerrarConexion()
+        {
+            if (lector != null && !lector.IsClosed)
+            {
+                lector.Close();
+            }
+            if (conection != null && conection.State != System.Data.ConnectionState.Closed)
+            {
+                conection.Close();
+            }
+        }
+    }
+}
---
+++ viejo.md
@@ -0,0 +1,31 @@
+NetPulse es una aplicacion desarrollada con C# utilizando .NET y una base de datos SQL, diseñada específicamente para una empresa de servicios de internet. Esta aplicación ofrece las siguientes funcionalidades:
+
+- *Administración de clientes*: Permite llevar a cabo operaciones de Alta, Baja y Modificación (ABM) de clientes, incluyendo la gestión de su información personal y de contacto.
+
+- *Gestión de servicios*: Permite realizar el ABM de servicios instalados para cada cliente, lo que incluye información detallada sobre los servicios contratados por los usuarios.
+
+- *Control de planes de internet*: Ofrece la posibilidad de gestionar los distintos planes de internet ofrecidos por la empresa, incluyendo velocidades y características específicas de cada uno.
+
+- *Diferentes niveles de usuarios*: La aplicación cuenta con varios niveles de acceso y permisos, tales como administrador y técnicos, cada uno con vistas y capacidades distintas adaptadas a sus roles.
+
+- *Administración de instalaciones y servicios técnicos*: Permite gestionar la instalación y desinstalación de servicios de internet, así como también brinda funcionalidades para agendar y resolver solicitudes de servicio técnico por parte de los clientes.
+
+LOGIN
+![image](https://github.com/LeonasZ/NetPulse/assets/106259939/df0167d7-10bb-4408-88ba-75a451ba8d77)
+
+Vista ADMINISTRADOR
+Pestaña Clientes
+![image](https://github.com/LeonasZ/NetPulse/assets/106259939/7aef3a20-438f-4795-afcc-b172e853f8de)
+Pestaña Servicios
+![image](https://github.com/LeonasZ/NetPulse/assets/106259939/381fb540-8562-40af-b774-091de0e113ba)
+Pestaña Mantenimientos
+![image](https://github.com/LeonasZ/NetPulse/assets/106259939/1008672e-d543-4a5f-9dce-66edf981e13a)
+Pestaña Actividad
+![image](https://github.com/LeonasZ/NetPulse/assets/106259939/2df58548-aaa9-425d-be53-dd5a9191bebd)
+
+Vista TÉCNICO
+![image](https://github.com/LeonasZ/NetPulse/assets/106259939/0181f1fc-80fd-4da3-b051-ef6a02b6fb96)
+
+
+
+
